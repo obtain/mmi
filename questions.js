9037f5ae5113fc094c27eb593cd1db54d94ae8dc
jd3johns <jonathan.david.johnston@gmail.com>
2016-02-16 22:10:41 -0500

    Add some questions and clean up some code.

--- questions.js
+++ questions.js
@@ -10,6 +10,85 @@ var MMIQuestions = [
         "What is that which there is the then knowin that you are dead?"
     ],
     "tags": ["care","ethics","medicine","law"]
+}, {
+    "question": "Dr. Curt recommends homeopathic medicines to his patients. There is no scientific evidence or widely accepted theory to suggest that homeopathic medicines work, and Dr. Curt doesn't believe in them. He recommends homeopathic medicine to people with mild and non-specific symptoms such as fatigue, headaches, and muscle aches, because he believes that it will do no harm, but will give them reassurance. What ethical situations does Dr. Curt's behavior pose?",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "A 2 year old infant has lost a massive amount of blood in a road accident and again the best chance of saving the child's life is an urgent blood transfusion and operative intervention to arrest the bleeding. Both the child's parents are Jehovah's Witnesses and refuse to give permission for a blood transfusion, requesting instead that the best available non-blood products are used to restore volume and that surgery is carried out without blood. They understand that this will be a far more dangerous course of action than surgery plus blood transfusion but persist in refusing permission for a blood transfusion for their child. How would you proceed?",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "Due to the shortage of medical professionals in rural communities, it has been suggested that programs preferentially admit students who are willing to commit to a 2 or 3 year tenure in an under-serviced area upon graduation. What are the broad implications of this policy for health and health care costs? Would it be effective? At what expense?",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "A close friend in your 1st-year medical school class tells you that his mother was recently diagnosed with breast cancer. He feels overwhelmed by his studies and is considering dropping out of medical school to spend more time with his mother. How do you counsel your friend?",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "\"Liberation Therapy\" is a vascular operation developed to potentially cure multiple sclerosis in certain patients, has recently come under very serious criticism - delaying widespread use. Among other experimental flaws, critics cite a small sample size in the original evidence used to support LT. As a healthcare policy maker, your job is to weigh the pros and cons in approving novel drugs and therapies. Discuss the issues you would consider during an approval process for LT.",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "Clostridium Difficile (D. difficile) is a type of bacteria that increases its activity with most antibiotic use, and it is therefore very difficult to treat. Research shows that the most effective way to prevent the spread of infection is frequent handwashing. However, many people have flat-out refused to wash their hands in hospitals. The governments is contemplating passing a policy to make it mandatory for people entering hospitals to wash their hands or else risk not being seen by doctors and being escorted out of the building against their will. Do you think the government should go ahead with this plan. Discuss the legal, ethical or practical problems that exist.",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "You are on the committee for selecting a new Dean of Science. What characteristics and/or qualities would you look for when selecting an effective dean?",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "",
+    "probing_questions": [],
+    "tags": []
+}, {
+    "question": "",
+    "probing_questions": [],
+    "tags": []
 }
 ];
-
