263ed67ed9f75875d7155f370f86efa5baa5bfa0
jd3johns <jonathan.david.johnston@gmail.com>
2016-02-23 21:02:57 -0500

    Fix boundary bugs and fix end of questions message to hide timer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,123 +5,119 @@ var question_number = -1;
 var question_indices;
 
 $( document ).ready(function() {
-	$('.question').hide();
-	// Create array of indices for the question set
-	question_indices = Array.apply(null, Array(questions.length)).map(
-	function (_, i) {return i;}
-	);
+  $('.question').hide();
+  // Create array of indices for the question set
+  question_indices = Array.apply(null, Array(questions.length)).map(
+  function (_, i) {return i;}
+  );
 
-	// Randomize the indices in linear time
-	shuffle(question_indices);
+  // Randomize the indices in linear time
+  shuffle(question_indices);
 });
 
 $(document).keydown(function(e){
-	if(e.keyCode == 13) {
-		change_state();
-	}else if (e.keyCode == 37){
-		console.log('Left');
-		question_number -= (question_number > 0) ? 1 : 0;
-		next_question();
-	}else if (e.keyCode == 39){
-		console.log('Right');
-		question_number += (question_number < question_indices.length) ? 1 : 0;
-		next_question(); 
-	}
+  if(e.keyCode == 13) {
+    change_state();
+  }else if (e.keyCode == 37 && question_number > -1){
+    question_number -= (question_number > 0) ? 1 : 0;
+    next_question();
+  }else if (e.keyCode == 39){
+    question_number += (question_number < questions.length) ? 1 : 0;
+    next_question(); 
+  }
 });
 
 function change_state(){
-	var state = $('.question').attr('data-state');
+  var state = $('.question').attr('data-state');
 
-	if(state == "prep"){
-		start_countdown(8);
-		$('.action').html("to go to next question");
-		$('.question').attr('data-state', 'answer');
-		$('#state').html("Time left to answer:");
-	}else if(state == 'answer'){
-		question_number++;
-		next_question();
-	}else{
-		question_number++;
-		next_question();
-	}
+  if(state == "prep"){
+    start_countdown(8);
+    $('.action').html("to go to next question");
+    $('.question').attr('data-state', 'answer');
+    $('#state').html("Time left to answer:");
+  }else if (question_number < questions.length) { // answer or null
+    question_number++;
+    next_question();
+  }
 }
 
 function next_question(){
-	start_countdown(2);
-	$('.action').html("to answer the question");
-	$('.question').attr('data-state', 'prep');
-	$('#state').html("Time left to prepare:");
+  start_countdown(2);
+  $('.action').html("to answer the question");
+  $('.question').attr('data-state', 'prep');
+  $('#state').html("Time left to prepare:");
 
-	display_question();
+  display_question();
 
-	$('.question').show();
-	$('.instructions').show();
-	$('.intro').hide();
+  $('.question').show();
+  $('.instructions').show();
+  $('.intro').hide();
 }
 
 function start_countdown(time){
-	clearInterval(counter);
-	$('#timer').html(prettify_number(time) + ':00');
-	var start = new Date;
+  clearInterval(counter);
+  $('#timer').html(prettify_number(time) + ':00');
+  var start = new Date;
 
-	counter = setInterval(function(){
-		var end = new Date;
-		var negative = "";
+  counter = setInterval(function(){
+    var end = new Date;
+    var negative = "";
 
-		var seconds = Math.ceil((time * 60) - (end - start)/1000);
-		var minutes = Math.floor(Math.abs(seconds)/ 60);
+    var seconds = Math.ceil((time * 60) - (end - start)/1000);
+    var minutes = Math.floor(Math.abs(seconds)/ 60);
 
-		seconds = seconds % 60;
+    seconds = seconds % 60;
 
-		if( seconds < 0){
-			$('#timer').addClass("error");
-			negative = '-';
-		}else{
-			$('#timer').removeClass("error");
-		}
+    if( seconds < 0){
+      $('#timer').addClass("error");
+      negative = '-';
+    }else{
+      $('#timer').removeClass("error");
+    }
 
-		$('#timer').html(negative + prettify_number(minutes) + ':' + prettify_number(seconds));
+    $('#timer').html(negative + prettify_number(minutes) + ':' + prettify_number(seconds));
 
-	},1000);
+  },1000);
 }
 
 function prettify_number(number){
-	var number = Math.abs(number);
-	if(number < 10){
-		return ('0' + number);
-	}else{
-		return number;
-	}
+  var number = Math.abs(number);
+  if(number < 10){
+    return ('0' + number);
+  }else{
+    return number;
+  }
 }
 
 function display_question(){
-	var question = questions[question_indices[question_number]];
-	console.log(question_number);
-	console.log(question_indices[question_number]);
-	console.log(questions[question_indices[question_number]]);
-	if(question){
-		$('.text').html('('+ (question_number + 1)+') ' +question.question);
+  var question = questions[question_indices[question_number]];
+  if(question){
+    $('.text').html('('+ (question_number + 1)+') ' +question.question);
 
-		var src = source[question.source];
-		$('#source').html('<a href="'+src.link+'">' + src.institution.full + ", " + src.year +  '</a>');
-	}else{
-		$('.text').html('Congrats! You answered all the questions. <br>Come back again!');
-		$('#source').html('');
-	}
+    var src = source[question.source];
+    $('#source').html('<a href="'+src.link+'">' + src.institution.full + ", " + src.year +  '</a>');
+    $('#timer').show();
+    $('#state').show();
+  }else{
+    $('.text').html('Congrats! You answered all the questions. <br>Come back again!');
+    $('#source').html('');
+    $('#timer').hide();
+    $('#state').hide();
+  }
 }
 
 function shuffle(array) {
-    var tmp, i, m = array.length;
+  var tmp, i, m = array.length;
 
-    while (m) {
-        // Pick remaining element
-        i = Math.floor(Math.random() * m--);
+  while (m) {
+    // Pick remaining element
+    i = Math.floor(Math.random() * m--);
 
-        // Swap with current element from end
-        tmp = array[m];
-        array[m] = array[i];
-        array[i] = tmp;
-    }
+    // Swap with current element from end
+    tmp = array[m];
+    array[m] = array[i];
+    array[i] = tmp;
+  }
 
-    return array;
+  return array;
 }
